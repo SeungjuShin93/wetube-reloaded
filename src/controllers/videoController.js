3300cf8a012016b60a6bd7012634641c1628aa2a
Ted <shinsuengju1@gmail.com>
2023-10-30 15:33:09 +0900

    #6.21 Edit Video part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -29,9 +29,20 @@ export const getEdit = async (req, res) => {
   }
   return res.render('edit', { pageTitle: `Edit: ${video.title}`, video });
 };
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render('404', { pageTitle: 'Video not found' });
+  }
+  video.title = title;
+  video.description = description;
+  video.hashtags = hashtags.split(',').map((word) => {
+    const returnWord = word.trim();
+    return returnWord.startsWith('#') ? returnWord : `#${returnWord}`;
+  });
+  await video.save();
   return res.redirect(`/videos/${id}`);
 };
 export const getUpload = (req, res) => {
