1e32dd6ea0b268d382f4c5b3c163fa285d44135f
Ted <shinsuengju1@gmail.com>
2023-11-22 15:55:16 +0900

    #7.14 Expiration and Secrets

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://127.0.0.1:27017/wetube');
+mongoose.connect(process.env.DB_URL);
 
 const db = mongoose.connection;
 
 const handleOpen = () => console.log('✅ Connected to DB');
-const handleError = (error)  => console.log("DB Error", error);
-db.on("error", handleError);
-db.once("open", handleOpen);
+const handleError = (error) => console.log('DB Error', error);
+db.on('error', handleError);
+db.once('open', handleOpen);
