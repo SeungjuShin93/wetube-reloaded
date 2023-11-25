08faa1d13ffe10ea4e43415ee06acef7cb75634c
Ted <shinsuengju1@gmail.com>
2023-11-25 11:36:33 +0900

    #7.21 Github Login part Six

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -140,13 +140,30 @@ export const finishGithubLogin = async (req, res) => {
         },
       })
     ).json();
-    const email = emailData.find(
+    const emailObj = emailData.find(
       (email) => email.primary === true && email.verified === true
     );
-    if (!email) {
+    if (!emailObj) {
       return res.redirect('login');
     }
-    // 이제 여기 코드는 email이 있다면 무얼 해줄지
+    const existingUser = await User.findOne({ email: emailObj.email });
+    if (existingUser) {
+      req.session.loggedIn = true;
+      req.session.user = existingUser;
+      return res.redirect('/');
+    } else {
+      const user = await User.create({
+        name: userData.name,
+        username: userData.login,
+        email: emailObj.email,
+        password: '',
+        socialOnly: true,
+        location: userData.location,
+      });
+      req.session.loggedIn = true;
+      req.session.user = user;
+      return res.redirect('/');
+    }
   } else {
     return res.redirect('login');
   }
