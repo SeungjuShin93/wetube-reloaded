2d8af0989ff09cb7ad7985723c766bb28174ece4
SeungjuShin93 <shinsuengju1@gmail.com>
2023-12-09 18:59:39 +0900

    #8.9 Video Upload

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -44,12 +44,13 @@ export const getUpload = (req, res) => {
   return res.render('upload', { pageTitle: 'Upload Video' });
 };
 export const postUpload = async (req, res) => {
+  const { path: fileUrl } = req.file;
   const { title, description, hashtags } = req.body;
   try {
     await Video.create({
       title,
       description,
-      // hashtags: formatHashtags(hashtags),
+      fileUrl,
       hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect('/');
