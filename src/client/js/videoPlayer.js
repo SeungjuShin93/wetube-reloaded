0fb2d360d6fa47b98f7cb8d4d90b6decb8157fc4
SeungjuShin93 <shinsuengju1@gmail.com>
2023-12-17 15:19:53 +0900

    #11.3 Volume

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -3,6 +3,8 @@ const playBtn = document.getElementById('play');
 const muteBtn = document.getElementById('mute');
 const time = document.getElementById('time');
 const volumeRange = document.getElementById('volume');
+let volumeValue = 0;
+video.volume = volume;
 
 const handlePlayClick = (e) => {
   if (video.paused) {
@@ -20,8 +22,21 @@ const handleMute = (e) => {
     video.muted = true;
   }
   muteBtn.innerText = video.muted ? 'Unmute' : 'Mute';
-  volumeRange.value = video.muted ? 0 : 0.5;
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleVolumeChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = 'Mute';
+  }
+  volumeValue = value;
+  video.volume = value;
 };
 
 playBtn.addEventListener('click', handlePlayClick);
 muteBtn.addEventListener('click', handleMute);
+volumeRange.addEventListener('input', handleVolumeChange);
